4bd646ae7455f31d761c6aca85d6a059d26f8caa
artm22es <artm22es@gmail.com>
2023-10-18 12:22:10 +0200

    update

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import contacts from "../data.json";
+
+export class ContactForm extends Component {
+    state = {
+        name: '',
+        number: ''
+    }
+
+
+}
